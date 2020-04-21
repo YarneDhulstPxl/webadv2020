ae40b2fbfc3786683f583e22b3396450000c448d
Yarne D'hulst <yarne@pop-os.localdomain>
2020-04-21 19:11:04 +0200

    added local folder week2

---
+++ Week2/voorbeelden/code/vb8_herwerkt/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+        <Greeter name="Tim" background-color="red" />
+    </div>
+</template>
+
+<script>
+import Greeter from './components/Greeter.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Greeter
+  }
+}
+</script>
---
+++ Week2/voorbeelden/code/vb8_herwerkt/src/components/Greeter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div v-bind:style="computedStyle">Hello {{ name }}</div>
+</template>
+<script>
+export default {
+    name: 'Greeter',
+    props: [ 'backgroundColor', 'name'],
+    computed: {
+        computedStyle() {
+            return `background-color: ${this.backgroundColor}`;
+        }
+    }
+}
+</script>
+
